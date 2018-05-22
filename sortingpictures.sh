3f29065afe948a3560df16fe9625d23800ad734f
Milan <m.d.lamain@st.hanze.nl>
2018-05-22 17:17:42 +0200

    Bestaande directories wordt doorheen geloopt en de files erin worden teruggegeven

--- sortingpictures.sh
+++ sortingpictures.sh
@@ -1,17 +1,25 @@
 # This program allows to sort pictures into different subfolders based on the date.
 
-DEFAULTDIR='~/Pictures'
-
 printf "Input directory where the pictures are stored:"
-read $1
+read directory
 
-printf "How do the pictures need to be sorted?: (type week or month)"
+printf "How do the pictures need to be sorted? (type week or month):"
 read worm
 
-dir=$1 #Store user input directory in the variable
+dir=$directory   #$directory #Store user input directory in the variable
 
-if [ -d "$1"];
+if [ -d ~/"$directory" ] #If userinput is a directory continue else exit
+then echo "Directory is found"	
+else
+	echo "Directory not found script aborting."
+	exit
+fi
 
+#Loop through directory
+for i in ~/$dir/*
+do
+	echo file: $i
+done
 
 # mkdir to create folders. cp to copy files. mv used to move and/or rename files.
 
