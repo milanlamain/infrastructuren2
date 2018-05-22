7901d85605e04c278c0e83a2beb5f2037a73cb96
Milan <m.d.lamain@st.hanze.nl>
2018-05-22 10:25:02 +0200

    .

--- sortingpictures.sh
+++ sortingpictures.sh
@@ -6,20 +6,24 @@ read directory
 printf "How do the pictures need to be sorted? (type week or month):"
 read worm
 
-dir=$directory   #$directory #Store user input directory in the variable
+dir=$directory #Store user input directory in the variable
 
-if [ -d ~/"$directory" ] #If userinput is a directory continue else exit
+#USER variable is used to see which user is running the script
+if [ -d /home/$USER/"$directory" ] #If userinput is a directory continue else exit
 then echo "Directory is found"	
 else
-	echo "Directory not found script aborting."
+	echo "Directory not found, script aborting."
 	exit
 fi
 
 #Loop through directory
-for i in ~/$dir/*
+for i in /home/$USER/$dir/*
 do
 	echo file: $i
 done
+
+filename="${i##*/}"
+echo $filename
 
 # mkdir to create folders. cp to copy files. mv used to move and/or rename files.
 
